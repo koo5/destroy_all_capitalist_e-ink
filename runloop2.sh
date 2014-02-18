59d35ce5c1c61a5fd5652b8fdd633bf1c4866e48
Your Name <you@example.com>
2014-02-18 11:08:49 +0100

    fuck scrot

--- runloop2.sh
+++ runloop2.sh
@@ -2,7 +2,11 @@ cd /var/www/r/tmp2
 
 while true; do
 
-    ~/bin/scrot temp.png  -u; #-q 1-100
+#    scrot temp.png  -u; #-q 1-100
+
+#	import -window `xdotool getwindowfocus -f` -frame temp.png
+	#https://wiki.archlinux.org/index.php/Taking_a_Screenshot
+	
     if ! diff temp.png diff.png; then
     	date
 		cp temp.png diff.png
